be9fef195f13214826f219977cfbb7283d1e84f7
Marckgit <marckgit@gmail.com>
2023-04-04 16:27:38 -0600

    'floor_good_view_and_begin_camera'

--- src/components/ground.jsx
+++ src/components/ground.jsx
@@ -1,15 +1,17 @@
 import { usePlane } from '@react-three/cannon'
-
+import { groundTexture } from '../assets/textures'
 export function Ground () {
   const [ref] = usePlane(() => ({
     rotation: [-Math.PI / 2, 0, 0],
     position: [0, -0.5, 0]
   }))
 
+  groundTexture.repeat.set(100, 100)
+
   return (
     <mesh ref={ref}>
       <planeBufferGeometry attach='geometry' args={[100, 100]} />
-      <meshStandardMaterial attach='material' color='green' />
+      <meshStandardMaterial attach='material' map={groundTexture} />
     </mesh>
   )
 }
